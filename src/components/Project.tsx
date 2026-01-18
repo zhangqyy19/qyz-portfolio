b1e187d74456df42b0c7a5048f5afca6ae5ef1ca
Qian Yun Zhang <zhangqy@umich.edu>
2026-01-18 13:42:47 -0500

    updated resume and projects section

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -15,8 +15,10 @@ function Project() {
     return(
     <div className="projects-container" id="projects">
         <h1>Personal Projects</h1>
+        
         <div className="projects-grid">
-            <div className="project">
+            <h2>Demos coming soon!</h2>
+            {/* <div className="project">
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><img src={mock10} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.filmate.club/" target="_blank" rel="noreferrer"><h2>Filmate AI</h2></a>
                 <p>Developed movie finder app with semantic search and sentiment analysis using OpenAI GPT-3.5 Turbo, Qdrant, React, and Flask.</p>
@@ -25,7 +27,7 @@ function Project() {
                 <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><img src={mock09} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://yujisatojr.itch.io/highspeedchase" target="_blank" rel="noreferrer"><h2>High Speed Chase</h2></a>
                 <p>Designed, developed, and launched a 3D multiplayer racing game with C# and Unity. This is available on Itch.io for gamers worldwide to enjoy.</p>
-            </div>
+            </div> */}
         </div>
     </div>
     );
